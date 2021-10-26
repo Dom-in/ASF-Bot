a125aa58eec70f77148bcd01fb07714f5c85a187
Dominik <55330408+Dom-in@users.noreply.github.com>
2021-10-26 16:04:45 +0200

    Upload
    + All Files From Local Machine

---
+++ commands/botinfo.js
@@ -0,0 +1,60 @@
+const Discord = require('discord.js');
+const config = require("../configs/config.json");
+const fetch = require('node-fetch');
+const { NumberToCurrency } = require('../currency.json')
+module.exports = {
+    name: "botinfo",
+    run: async (bot, message, args, modules, BotVersion) => {
+        let name = args[0]
+        if(!name) return message.channel.send('**[ERROR]** You did not mention bots name!');
+        fetch('http://'+config.secruity.IP+'/Api/Bot/'+name+'?password='+config.secruity.ASF_PASSWORD)
+        .then(res => res.json())
+        .then(json => {
+        if(!json.Result[name]) return message.channel.send('**[ERROR]** '+name+' is not a valid bots name!');
+        let data = json.Result[name]
+        
+        //Start CardsFarmer Functions
+        let CurrentGamesFarming = data.CardsFarmer.CurrentGamesFarming
+        let TPT = '00:00:00'
+
+        if(CurrentGamesFarming.length == 0) {
+            CurrentGamesFarming = "No Games To Idle"
+        } else {
+            CurrentGamesFarming = CurrentGamesFarming[0].GameName+' | '+CurrentGamesFarming[0].CardsRemaining+" Cards"
+            TPT = CurrentGamesFarming[0].HoursPlayed
+        }
+        let Paused = data.CardsFarmer.Paused
+        if(Paused) {
+            Paused = "Yes"
+        } else {
+            Paused = "No"
+        }
+        let TimeRemaining = data.CardsFarmer.TimeRemaining
+        //Stop CardsFarmer Functions
+
+        //Start BotInfo Functions
+        let HasMobileAuthenticator = data.HasMobileAuthenticator
+            if(HasMobileAuthenticator) {
+                HasMobileAuthenticator = "Yes"
+            } else {
+                HasMobileAuthenticator = "No"
+            }
+        let IsConnectedAndLoggedOn = data.IsConnectedAndLoggedOn
+            if(IsConnectedAndLoggedOn) {
+                IsConnectedAndLoggedOn = "Yes"
+            } else {
+                IsConnectedAndLoggedOn = "No"
+            }
+        //Stop BotInfo Functions
+            console.log(data)
+        const BotInfo = new Discord.MessageEmbed()
+            .setAuthor(data.BotName, `https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/31/${data.AvatarHash}_full.jpg`)
+            .addField('__**Bot Info**__', `Balance: ${data.WalletBalance/100} ${NumberToCurrency[data.WalletCurrency]}\nOnline: ${IsConnectedAndLoggedOn}\nRedeem In Background: ${data.GamesToRedeemInBackgroundCount}\nMobile Authenticator: ${HasMobileAuthenticator}`, true)
+            .addField('__**Card Farming**__', `Current Game: ${CurrentGamesFarming}\nGame Play Time: ${TPT}\nTotal Time Remaining: ${TimeRemaining}\nPaused: ${Paused}`, true)
+            .setColor('AQUA')
+            .setTimestamp()
+            .setFooter(`ASF-Bot ${BotVersion}`, 'https://cdn.discordapp.com/icons/267292556709068800/49f0d4a116dfab2b6c5ad92c079c9070.png?size=512');
+        message.channel.send({ embeds: [BotInfo] });
+        })  
+    }
+}
---
+++ index.js
@@ -0,0 +1,74 @@
+const Discord = require('discord.js');
+const config = require('./configs/config.json');
+const client = new Discord.Client({
+        intents: ["GUILDS","GUILD_MEMBERS","GUILD_BANS", "GUILD_EMOJIS_AND_STICKERS", "GUILD_INTEGRATIONS", "GUILD_INVITES", "GUILD_VOICE_STATES", "GUILD_PRESENCES", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "DIRECT_MESSAGES"]
+});
+const Enmap = require('enmap')
+const fs = require('fs')
+const modules = require('./modules.js')
+const fetch = require('node-fetch');
+
+//Can be Changed/Updated
+const RconCommandsUse = ['redeem', 'addlicense', 'pause', 'stop', 'start', 'resume', 'restart', 'update', 'play'];
+const BotVersion = 'v0.0.1'
+//Can be Changed/Updated
+
+client.once('ready', () => {
+	console.log('[ASF-Bot] I am ready!');
+});
+
+client.on("ready", () => {     
+    client.commands = new Enmap();
+        fs.readdir("./commands/", (err, files) => {
+        if (err) return console.error(err);
+        files.forEach((file) => {
+        if (!file.endsWith(".js")) return;
+        let props = require(`./commands/${file}`);
+        let commandName = file.split(".")[0];
+        client.commands.set(commandName, props);
+    });
+});
+      
+client.on("message", async (message) => {
+        if (message.author.bot) return;
+        if (!message.author.id == config.secruity.UID) return;
+                if (message.channel.type === "dm") return;
+                        let prefix = config.bot.prefix;
+                        let messageArray = message.content.split(" ");
+                        let cmd = messageArray[0].toLowerCase();
+                        let args = messageArray.slice(1);
+                        if (!message.content.toLowerCase().startsWith(prefix)) return;
+
+                //CHECK IF NEEDED TO RUN IT IN BOT OR RCON
+        if(RconCommandsUse.includes(cmd.slice(prefix.length))) {
+                let command = {Command: message.content.slice(prefix.length)}
+                fetch("http://"+config.secruity.IP+"/Api/Command?password="+config.secruity.ASF_PASSWORD, {
+                        method: "post",
+                        body: JSON.stringify(command),
+                        headers: {"Content-Type": "application/json"}
+                }).then(res => res.json())
+                .then(body => {
+                        if (body.Success){
+                        const RconModuleEmbed = new Discord.MessageEmbed()
+                                .setColor('AQUA')
+                                .setAuthor('ASF-BOT Rcon Commands')
+                                .setDescription(body.Result)
+                                .setTimestamp()
+                                .setFooter(`ASF-Bot ${BotVersion}`, 'https://cdn.discordapp.com/icons/267292556709068800/49f0d4a116dfab2b6c5ad92c079c9070.png?size=512');
+                        message.channel.send({ embeds: [RconModuleEmbed] });
+
+                        } else {
+                        console.log("Error: " + body)
+                        }
+                })
+                //STOP CHECK IF NEEDED TO RUN IT IN BOT OR RCON
+        } else {
+
+                let commandfile = client.commands.get(cmd.slice(prefix.length));
+                if (commandfile) commandfile.run(client, message, args, modules, BotVersion);
+        }
+        })
+})
+
+
+client.login(config.bot.token);
